1262db1981b90763834024c077159eb9a7f00a10
MyHg0497 <105546418+MyHg0497@users.noreply.github.com>
2022-05-14 23:03:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Có phải Hương vợ anh ( •̀ ω •́ )✧',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Vợ sẵn sàng trả lời câu hỏi của anh chưa 😋 ',
+    desc: 'Chúc vợ anh yêu anh mãi không chán nè ❤',
     btnYes: 'Yêu anh nhiềuuuuuuu 😍',
-    btnNo: 'Anh mơ à 🙄',
+    btnNo: 'Anh đynn à 🙄',
     question: 'Hương, em yêu anh ở điểm gì dọ?',
     btnReply: '💘💘',
-    reply: 'Tại dzì anh cuteeeeeeeee',
-    mess: 'Anh yêu vợ quéeee 🥰. Tí mình call nhe. ',
-    messDesc: 'Vợ nhớ chụp nhiều ảnh với clip cho anh nhe ❤.',
+    reply: 'Tại dzì anh cuteeeeeeeee 😚',
+    mess: 'Vợ nhớ chụp nhiều ảnh với clip cho anh nhe ❤. ',
+    messDesc: 'Anh nghiện vợ quá rồi 😋😊.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
